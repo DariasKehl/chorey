48662b05c4aba3d9975a647f6e42f07378f1b78f
Jeffrey Lakin <neocount@gmail.com>
2021-04-08 16:26:26 -0400

    seed data now

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Darias", email: "neocount@gmail.com", password: "asdf")
+User.create(username: "jeff", email: "jeff@gmail.com", password: "jeff")
+User.create(username: "sean", email: "sean@gmail.com", password: "sean")
+User.create(username: "seamus", email: "seamus@gmail.com", password: "seamus")
+
+Task.create(name: "Vacuum", details: "vacuum floor carpet and hardwood, moving furniture. ", task_for: "Dining Room", user_id: 2)
+Task.create(name: "Clear Table", details: "School Items and old dishes", task_for: "Dining Room", user_id: 2)
+Task.create(name: "Laundry", details: "Clean up laundry/shoes ", task_for: "Dining Room", user_id: 2)
+Task.create(name: "Wash Windows", details: "Wash Windows inside dining room. ", task_for: "Dining Room", user_id: 2)
+Task.create(name: "Wash Windows", details: "Help sean wash the windows ", task_for: "Dining Room", user_id: 3)
+
+ChoreList.create(name: "Dining Room Daily", room: "Dining Room", user_id: 2)
+
+ChoreListTask.create(chore_list_id: 1, task_id: 4, user_id: 2)
+ChoreListTask.create(chore_list_id: 1, task_id: 4, user_id: 3)
